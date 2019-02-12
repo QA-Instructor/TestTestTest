acfe69520fa0756755b1ea7b7c2434abdf688343
Toria <QAInstructorSky@gmail.com>
2019-02-12 13:31:55 +0000

    Lisa changed to Bart Simpson

--- Ex6A_Lisa_Starter.php
+++ Ex6A_Lisa_Starter.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Lisa Marie Simpson</title>
+  <title>Bart Simpson</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="styles.css" rel="stylesheet" type="text/css"/>
